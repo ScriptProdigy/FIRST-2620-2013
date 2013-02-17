06f2b5e192bbb81144aa3d0b76c4341628471b00
frc2620 <frc2620@ubuntu>
2013-02-17 16:02:48 -0500

    Climber now has manual override in operator mode in case of emergency. Buttons 8+9 run override mode, where button 8 runs automated mode.

--- src/org/first/team2620/FRCRobot_2013.java
+++ src/org/first/team2620/FRCRobot_2013.java
@@ -49,7 +49,15 @@ public class FRCRobot_2013 extends SimpleRobot
             
             if(RobotMap.Joystick1.getRawButton(8))
             {
-                climber.climb();
+                if(RobotMap.Joystick1.getRawButton(8) & RobotMap.Joystick1.getRawButton(9)) {
+                    // Manual override of conveyors in case of emergencies. 
+                    
+                    climber.overRideClimb();
+                    manualConveyorControl();
+                } else {
+                    
+                    climber.climb();
+                }
             }
         }
     }
@@ -64,49 +72,7 @@ public class FRCRobot_2013 extends SimpleRobot
             getWatchdog().feed();
             RobotMap.drive.tankDrive(RobotMap.Joystick1.getY() * RobotMap.DriveDirection, RobotMap.Joystick2.getY() * RobotMap.DriveDirection);
             
-            if(RobotMap.Joystick1.getRawButton(11))
-            {
-                RobotMap.Leg.set(1);
-            }
-            else
-            {
-                if(RobotMap.Joystick1.getRawButton(10))
-                {
-                    RobotMap.Leg.set(-1);
-                }
-                else
-                {
-                    RobotMap.Leg.set(0);
-                }
-            }
-            
-            if(RobotMap.Joystick1.getRawButton(6)) {
-                RobotMap.DriveDirection = 1;
-            }
-            
-            if(RobotMap.Joystick1.getRawButton(7)) {
-                RobotMap.DriveDirection = -1;
-            }
-            
-            double ConveyorSpeed = (RobotMap.Joystick1.getThrottle() + 1) / 2; // from (-1 to 1) to (1, 0)
-            if(RobotMap.Joystick1.getRawButton(1)) {
-                RobotMap.LHConveyor.set(ConveyorSpeed);
-                RobotMap.RHConveyor.set(-ConveyorSpeed);
-                System.out.println("Conveyor Speed: " + ConveyorSpeed);
-            }
-            else
-            {
-                if(RobotMap.Joystick2.getRawButton(1)) {
-                    RobotMap.LHConveyor.set(-ConveyorSpeed);
-                    RobotMap.RHConveyor.set(ConveyorSpeed);
-                    System.out.println("Conveyor Speed: " + ConveyorSpeed);
-                }
-                else
-                {
-                    RobotMap.LHConveyor.set(0);
-                    RobotMap.RHConveyor.set(0);
-                }
-            }
+            manualConveyorControl();
             
             if(RobotMap.Joystick2.getRawButton(8)) {
                 RobotMap.ShooterWheel.set(0.75);
@@ -115,5 +81,49 @@ public class FRCRobot_2013 extends SimpleRobot
             }
         }
         LiveWindow.setEnabled(false);
+    }
+    
+    public void manualConveyorControl()
+    {
+        if(RobotMap.Joystick1.getRawButton(11)) {
+            RobotMap.Leg.set(1);
+        }
+        else
+        {
+            if(RobotMap.Joystick1.getRawButton(10)) {
+                RobotMap.Leg.set(-1);
+            }
+            else {
+                RobotMap.Leg.set(0);
+            }
+        }
+
+        if(RobotMap.Joystick1.getRawButton(6)) {
+            RobotMap.DriveDirection = 1;
+        }
+
+        if(RobotMap.Joystick1.getRawButton(7)) {
+            RobotMap.DriveDirection = -1;
+        }
+
+        double ConveyorSpeed = (RobotMap.Joystick1.getThrottle() + 1) / 2; // from (-1 to 1) to (1, 0)
+        if(RobotMap.Joystick1.getRawButton(1)) {
+            RobotMap.LHConveyor.set(ConveyorSpeed);
+            RobotMap.RHConveyor.set(-ConveyorSpeed);
+            System.out.println("Conveyor Speed: " + ConveyorSpeed);
+        }
+        else
+        {
+            if(RobotMap.Joystick2.getRawButton(1)) {
+                RobotMap.LHConveyor.set(-ConveyorSpeed);
+                RobotMap.RHConveyor.set(ConveyorSpeed);
+                System.out.println("Conveyor Speed: " + ConveyorSpeed);
+            }
+            else
+            {
+                RobotMap.LHConveyor.set(0);
+                RobotMap.RHConveyor.set(0);
+            }
+        }
     }
 }
--- src/org/first/team2620/subsystems/Climber.java
+++ src/org/first/team2620/subsystems/Climber.java
@@ -10,146 +10,169 @@ public class Climber {
     
     private int LevelCount_ = 0;
     private int StopClimbLevel_ = 3;
-    private double LHClimbPower = RobotMap.ClimbPower;
-    private double RHClimbPower = -RobotMap.ClimbPower;
+    private double LHClimbPower_ = RobotMap.ClimbPower;
+    private double RHClimbPower_ = -RobotMap.ClimbPower;
     private double LegPower = RobotMap.LegPower;
     private boolean LHClimb = true;
     private boolean RHClimb = true;
+    
+    private Thread ConveyorsThread_ = null;
+    private Thread LegThread_ = null;
+    
+    private boolean End_ = false;
+    
+    private boolean Climbing_ = false;
 
     public void climb()
     {
-        // Conveyors Movement Thread
-        new Thread(new Runnable() {
+        if(Climbing_ == false)
+        {
+            Climbing_ = true;
+            
+            // Conveyors Movement Thread
+            ConveyorsThread_ = new Thread(new Runnable() {
 
-            public void run() {
-                
-                while(LevelCount_ <= StopClimbLevel_)
-                {
-                    if(RobotMap.LHTopHooked.get() && RobotMap.RHTopHooked.get()) // Both are on top hook
+                public void run() {
+
+                    while(LevelCount_ <= StopClimbLevel_ && End_ == false)
                     {
-                        LHClimb = true;
-                        RHClimb = true;
-                        
-                        LHClimbPower *= -1;
-                        RHClimbPower *= -1;
-                        LevelCount_ += 1;
-                    }
-                    else
-                    {
-                        if(RobotMap.LHMiddleHooked.get() && RobotMap.RHMiddleHooked.get()) // Both holding on middle
+                        if(RobotMap.LHTopHooked.get() && RobotMap.RHTopHooked.get()) // Both are on top hook
                         {
                             LHClimb = true;
                             RHClimb = true;
-                        
-                            if(LevelCount_ == StopClimbLevel_)
+
+                            LHClimbPower_ *= -1;
+                            RHClimbPower_ *= -1;
+                            LevelCount_ += 1;
+                        }
+                        else
+                        {
+                            if(RobotMap.LHMiddleHooked.get() && RobotMap.RHMiddleHooked.get()) // Both holding on middle
                             {
-                                LHClimb = false;
-                                RHClimb = false;
-                            } 
+                                LHClimb = true;
+                                RHClimb = true;
+
+                                if(LevelCount_ == StopClimbLevel_)
+                                {
+                                    LHClimb = false;
+                                    RHClimb = false;
+                                } 
+                                else
+                                {
+                                    LHClimbPower_ *= -1;
+                                    RHClimbPower_ *= -1;
+                                }
+                            }
+                            else // Middle of climbing, lets keep this bad boy level
+                            {
+                                // Level each side out at top
+                                if(RobotMap.LHTopHooked.get() && (RobotMap.RHTopHooked.get() == false)) // Left hand is ready to stop, right hand keep climbing to top
+                                {
+                                    LHClimb = false;
+                                } 
+                                else if(RobotMap.RHTopHooked.get() && (RobotMap.LHTopHooked.get() == false)) // Right hand is ready to stop, left hand keep climbing to top
+                                {
+                                    RHClimb = false;
+                                }
+
+                                // Level each side out at middle
+                                if(RobotMap.LHMiddleHooked.get() && (RobotMap.RHMiddleHooked.get() == false)) // Left hand is ready to stop, right hand keep climbing to top
+                                {
+                                    LHClimb = false;
+                                } 
+                                else if(RobotMap.RHMiddleHooked.get() && (RobotMap.LHMiddleHooked.get() == false)) // Right hand is ready to stop, left hand keep climbing to top
+                                {
+                                    RHClimb = false;
+                                }
+                            }
+                        }
+
+                        if(LHClimb) {
+                            RobotMap.LHConveyor.set(LHClimbPower_);
+                        } else {
+                            RobotMap.LHConveyor.set(0);
+                        }
+
+                        if(RHClimb) {
+                            RobotMap.RHConveyor.set(RHClimbPower_);
+                        } else {
+                            RobotMap.RHConveyor.set(0);
+                        }
+                    }
+
+                    // Just to make sure we stop climbing
+                    RobotMap.LHConveyor.set(0);
+                    RobotMap.RHConveyor.set(0);
+                }
+
+            });
+
+            // Leg Movement Thread
+            LegThread_ = new Thread(new Runnable() {
+
+                public void run() {
+
+                    boolean bringDown = false;
+                    boolean bringUp = false;
+
+                    while(LevelCount_ < StopClimbLevel_ && End_ == false)
+                    {
+                        if(RobotMap.LHMiddleHooked.get() && RobotMap.RHMiddleHooked.get()) // Both holding on middle, bring leg down
+                        {
+                            bringDown = true;
+                        }
+
+                        if(RobotMap.LHTopHooked.get() && RobotMap.RHTopHooked.get()) // Both are set on top, bring leg up to get out of corners way
+                        {
+                            bringUp = true;
+                        }
+
+                        if(bringDown)
+                        {
+                            if(RobotMap.LegDown.get() == false)
+                            {
+                                RobotMap.Leg.set(LegPower);
+                            }
                             else
                             {
-                                LHClimbPower *= -1;
-                                RHClimbPower *= -1;
+                                RobotMap.Leg.set(0);
+                                bringDown = false;
                             }
                         }
-                        else // Middle of climbing, lets keep this bad boy level
-                        {
-                            // Level each side out at top
-                            if(RobotMap.LHTopHooked.get() && (RobotMap.RHTopHooked.get() == false)) // Left hand is ready to stop, right hand keep climbing to top
-                            {
-                                LHClimb = false;
-                            } 
-                            else if(RobotMap.RHTopHooked.get() && (RobotMap.LHTopHooked.get() == false)) // Right hand is ready to stop, left hand keep climbing to top
-                            {
-                                RHClimb = false;
-                            }
-                            
-                            // Level each side out at middle
-                            if(RobotMap.LHMiddleHooked.get() && (RobotMap.RHMiddleHooked.get() == false)) // Left hand is ready to stop, right hand keep climbing to top
-                            {
-                                LHClimb = false;
-                            } 
-                            else if(RobotMap.RHMiddleHooked.get() && (RobotMap.LHMiddleHooked.get() == false)) // Right hand is ready to stop, left hand keep climbing to top
-                            {
-                                RHClimb = false;
-                            }
-                        }
-                    }
-                    
-                    if(LHClimb) {
-                        RobotMap.LHConveyor.set(LHClimbPower);
-                    } else {
-                        RobotMap.LHConveyor.set(0);
-                    }
-                    
-                    if(RHClimb) {
-                        RobotMap.RHConveyor.set(RHClimbPower);
-                    } else {
-                        RobotMap.RHConveyor.set(0);
-                    }
-                }
-                
-                // Just to make sure we stop climbing
-                RobotMap.LHConveyor.set(0);
-                RobotMap.RHConveyor.set(0);
-            }
-            
-        }).start();
-    
-        // Leg Movement Thread
-        new Thread(new Runnable() {
 
-            public void run() {
-                boolean bringDown = false;
-                boolean bringUp = false;
-                while(LevelCount_ < StopClimbLevel_)
-                {
-                    if(RobotMap.LHMiddleHooked.get() && RobotMap.RHMiddleHooked.get()) // Both holding on middle, bring leg down
-                    {
-                        bringDown = true;
-                    }
-                    
-                    if(RobotMap.LHTopHooked.get() && RobotMap.RHTopHooked.get()) // Both are set on top, bring leg up to get out of corners way
-                    {
-                        bringUp = true;
-                    }
-                    
-                    if(bringDown)
-                    {
-                        if(RobotMap.LegDown.get() == false)
+                        if(bringUp)
                         {
-                            RobotMap.Leg.set(LegPower);
+                            if(RobotMap.LegUp.get() == false)
+                            {
+                                RobotMap.Leg.set(-LegPower);
+                            }
+                            else
+                            {
+                                RobotMap.Leg.set(0);
+                                bringDown = false;
+                            }
                         }
-                        else
-                        {
-                            RobotMap.Leg.set(0);
-                            bringDown = false;
-                        }
+
                     }
-                    
-                    if(bringUp)
+
+                    // Bring leg all the way up after it climbs all the way
+                    while(RobotMap.LegUp.get() == false && End_ == false)
                     {
-                        if(RobotMap.LegUp.get() == false)
-                        {
-                            RobotMap.Leg.set(-LegPower);
-                        }
-                        else
-                        {
-                            RobotMap.Leg.set(0);
-                            bringDown = false;
-                        }
+                        RobotMap.Leg.set(-LegPower);
                     }
-                        
+                    RobotMap.Leg.set(0);
                 }
-                
-                // Bring leg all the way up after it climbs all the way
-                while(RobotMap.LegUp.get() == false)
-                {
-                    RobotMap.Leg.set(-LegPower);
-                }
-                RobotMap.Leg.set(0);
-            }
-        } ).start();
+            });
+
+
+            ConveyorsThread_.start();
+            LegThread_.start();
+        }
+    }
+    
+    public void overRideClimb()
+    {
+        End_ = true;
     }
 
 }
