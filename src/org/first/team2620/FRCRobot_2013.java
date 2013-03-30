1c71e8a7ef9d5500be7f878434d571943edfa901
frc2620 <frc2620@ubuntu>
2013-03-29 20:28:47 -0400

    Attempted cleaned up code, hopefully everything still works!

--- src/org/first/team2620/FRCRobot_2013.java
+++ src/org/first/team2620/FRCRobot_2013.java
@@ -14,15 +14,12 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.first.team2620.subsystems.Climber;
 import org.first.team2620.subsystems.Shooter;
 
 
 public class FRCRobot_2013 extends SimpleRobot 
 {
     public Shooter shooter;
-    public Climber climber;
-    public boolean ManualClimb = false;
     public SendableChooser auton;
     
     
@@ -36,14 +33,18 @@ public class FRCRobot_2013 extends SimpleRobot
     public void robotInit()
     {
         auton = new SendableChooser();
-        auton.addDefault("Back Left", (Object)String.valueOf(ShooterPositions[0]));
-        auton.addObject("Back Right", (Object)String.valueOf(ShooterPositions[1]));
-        auton.addObject("Front Left", (Object)String.valueOf(ShooterPositions[2]));
-        auton.addObject("Front Right", (Object)String.valueOf(ShooterPositions[3]));
+        auton.addDefault("Back Left", (Object)String.valueOf(0));
+        auton.addObject("Back Right", (Object)String.valueOf(1));
+        auton.addObject("Front Left", (Object)String.valueOf(2));
+        auton.addObject("Front Right", (Object)String.valueOf(3));
         SmartDashboard.putData("Autonomous Mode", auton);
         
+        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+        
         shooter = new Shooter();
-        climber = new Climber();
     }
     
     public void autonomous() 
@@ -52,8 +53,7 @@ public class FRCRobot_2013 extends SimpleRobot
         RobotMap.ShooterWheel.set(1);
         RobotMap.DiskInsert.set(0.1);
         
-        int valueReq = Integer.parseInt(auton.getSelected().toString()); //291;
-        System.out.println("Auton Angle Needed: " + valueReq);
+        int posValue = Integer.parseInt(auton.getSelected().toString());
         
         int shootCount = 0;
         int totalShootCount = 6;
@@ -63,90 +63,58 @@ public class FRCRobot_2013 extends SimpleRobot
             getWatchdog().feed();
             
             RobotMap.drive.tankDrive(0, 0);
-            //System.out.println("VALUE: " + RobotMap.ShooterAngle.getValue());
             
-            
-            int angle = RobotMap.ShooterAngle.getValue();
-            boolean belowThreshold = valueReq > (angle-(threshold/2));
-            boolean aboveThreshold = (angle+(threshold/2)) > valueReq;
-            
-            if(belowThreshold)
+            if(shooterMoveToPosition(posValue))
             {
-                //System.out.println("Lift Up");
-                shooter.liftUp();
-            }
-            else
-            {
-                /*if(aboveThreshold)
+                if((shootCount < totalShootCount))
                 {
-                    System.out.println("Lift Down");
-                    shooter.liftDown();
+                    try {
+
+                        Thread.sleep(1000);
+                        RobotMap.DiskInsert.set(1);
+                        Thread.sleep(1000);
+                        RobotMap.DiskInsert.set(0.1);
+
+                        shootCount += 1;
+
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
                 }
                 else
-                {*/
-                    shooter.stopLift();
-                    
-                    if(shootCount < totalShootCount)
-                    {
-                        try {
-                            
-                            Thread.sleep(1000);
-                            RobotMap.DiskInsert.set(1);
-                            Thread.sleep(1000);
-                            RobotMap.DiskInsert.set(0.1);
-                            
-                            shootCount += 1;
-                            
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                    else
-                    {
-                        RobotMap.ShooterWheel.set(0);
-                    }
-                //}
+                {
+                    RobotMap.ShooterWheel.set(0);
+                }
             }
+            
+            Timer.delay(0.05);
         }
+           
     }
-    // 243
-    // 397
 
-    public void moveToPosition(int pos)
+    public boolean shooterMoveToPosition(int pos) // Returns true if at position
     {
-    /*  0 = Back Left
-         *   1 = Back Right
-         *   2 = Front Left
-         *   3 = Front Right
-         */
-        int valueReq = pos;
+        int valueReq = ShooterPositions[pos];
         
         int angle = RobotMap.ShooterAngle.getValue();
         boolean belowThreshold = valueReq > (angle-(threshold/2));
         boolean aboveThreshold = (angle+(threshold/2)) > valueReq;
         
-        if(Teleop_MoveUp)
-        {
-            if(belowThreshold)
-            {
-                //System.out.println("Lift Up");
+        if(Teleop_MoveUp) {
+            if(belowThreshold) {
                 shooter.liftUp();
-            }
-            else
-            {
+                return false;
+            } else {
                 shooter.stopLift();
+                return true;
             }
-        }
-        else
-        {
-            if(aboveThreshold)
-            {
-                //System.out.println("Lift Down");
+        } else {
+            if(aboveThreshold) {
                 shooter.liftDown();
-            }
-            else
-            {
+                return false;
+            } else {
                 shooter.stopLift();
+                return true;
             }
         }
         
@@ -154,18 +122,14 @@ public class FRCRobot_2013 extends SimpleRobot
     
     public void operatorControl() 
     {
-        climber.newMatch();
         shooter.newMatch();
         
         while(isOperatorControl() && isEnabled())
         {
             getWatchdog().feed();
             
-            //System.out.println("SHOOTER VALUE  ENCODER: " + RobotMap.ShooterAngle.getValue());
-            //System.out.println("SHOOTER ANALOG ENCODER: " + RobotMap.ShooterAngle.getVoltage());
-            
             // Drive
-            drive();
+            RobotMap.drive.arcadeDrive(RobotMap.Joystick2);
             
             // Shooter Lift
             
@@ -175,19 +139,19 @@ public class FRCRobot_2013 extends SimpleRobot
                 Teleop_Move = true;
                 if(RobotMap.Joystick2.getRawButton(6))
                 {
-                    Teleop_MoveToPosition = ShooterPositions[0];
+                    Teleop_MoveToPosition = 0;
                 }
                 else if(RobotMap.Joystick2.getRawButton(7))
                 {
-                    Teleop_MoveToPosition = ShooterPositions[1];
+                    Teleop_MoveToPosition = 1;
                 }
                 else if(RobotMap.Joystick2.getRawButton(10))
                 {
-                    Teleop_MoveToPosition = ShooterPositions[2];
+                    Teleop_MoveToPosition = 2;
                 }
                 else if(RobotMap.Joystick2.getRawButton(11))
                 {
-                    Teleop_MoveToPosition = ShooterPositions[3];
+                    Teleop_MoveToPosition = 3;
                 }
                 
                 if(RobotMap.ShooterAngle.getValue() > Teleop_MoveToPosition)
@@ -220,29 +184,22 @@ public class FRCRobot_2013 extends SimpleRobot
             
             if(Teleop_Move)
             {
-                moveToPosition(Teleop_MoveToPosition);
+                shooterMoveToPosition(Teleop_MoveToPosition);
             }
             
-            if(RobotMap.Joystick2.getRawButton(4))
-            {
+            if(RobotMap.Joystick2.getRawButton(4)) { // Disk Insert
                 RobotMap.DiskInsert.set(1);
-            }
-            else
-            {
+            } else {
                 RobotMap.DiskInsert.set(0.1);
             }
             
-            if(RobotMap.Joystick2.getRawButton(1))
-            {
+            if(RobotMap.Joystick2.getRawButton(1)) { // Spin up shooter
                 shooter.speedUp();
-            }
-            else
-            {
+            } else {
                 shooter.stop();
             }
 
-            manualConveyorControl();
-            
+            legControl();
             Timer.delay(0.05);
         }
     }
@@ -257,14 +214,8 @@ public class FRCRobot_2013 extends SimpleRobot
         {
             getWatchdog().feed();
             
-            drive();
-           manualConveyorControl();
-            
-            if(RobotMap.Joystick2.getRawButton(2)) {
-                RobotMap.ShooterWheel.set(0.75);
-            } else {
-                RobotMap.ShooterWheel.set(0);
-            }
+            RobotMap.drive.arcadeDrive(RobotMap.Joystick2);
+            legControl();
             
             Timer.delay(0.05);
         }
@@ -272,13 +223,12 @@ public class FRCRobot_2013 extends SimpleRobot
     }
 
     
-    public void manualConveyorControl()
+    public void legControl()
     {
         
         boolean legNotUp = RobotMap.LegUp.get();
         boolean legNotDown = RobotMap.LegDown.get();
         if(RobotMap.Joystick2.getRawButton(8)) {
-            System.out.println("LEG UP: " + legNotUp);
             if(legNotUp)
             {
                 RobotMap.Leg.set(1);
@@ -287,7 +237,6 @@ public class FRCRobot_2013 extends SimpleRobot
         else
         {
             if(RobotMap.Joystick2.getRawButton(9)) {
-                System.out.println("LEG DOWN: " + legNotDown);
                 if(legNotDown)
                 {
                     RobotMap.Leg.set(-1);
@@ -298,50 +247,5 @@ public class FRCRobot_2013 extends SimpleRobot
             }
         }
 
-        /*double ConveyorSpeed = 1;
-        if(RobotMap.Joystick1.getRawButton(11))
-        {
-            RobotMap.RHConveyor.set(-ConveyorSpeed);
-        }
-        else 
-        {
-            if(RobotMap.Joystick1.getRawButton(10)) 
-            {
-                RobotMap.RHConveyor.set(ConveyorSpeed);
-            }
-            else
-            {
-                RobotMap.RHConveyor.set(0);
-            }
-        }
-        
-        if(RobotMap.Joystick1.getRawButton(6)) 
-        {
-            RobotMap.LHConveyor.set(ConveyorSpeed);
-        }
-        else
-        {
-            if(RobotMap.Joystick1.getRawButton(7)) 
-            {
-                RobotMap.LHConveyor.set(-ConveyorSpeed);
-            }
-            else
-            {
-                RobotMap.LHConveyor.set(0);
-            }
-        }*/
     }
-    
-    public void drive()
-    {
-        // RobotMap.drive.tankDrive(-RobotMap.Joystick1.getY(), -RobotMap.Joystick2.getY());
-        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
-        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
-        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-        RobotMap.drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
-        
-        RobotMap.drive.arcadeDrive(RobotMap.Joystick2);
-    }
-
-
 }
--- src/org/first/team2620/RobotMap.java
+++ src/org/first/team2620/RobotMap.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Victor;
-import org.first.team2620.subsystems.ConveyorProxSwitch;
 
 /**
  *
@@ -26,19 +25,8 @@ public class RobotMap {
     public static int DriveDirection = 1; // 1, or -1
     public static RobotDrive drive = new RobotDrive(LHDrive, RHDrive);
     
-    
-    // Climbing
-    public static Jaguar LHConveyor = new Jaguar(3);
-    public static ConveyorProxSwitch LHTop = new ConveyorProxSwitch(1);
-    public static ConveyorProxSwitch LHMiddle = new ConveyorProxSwitch(2); //new DigitalInput(0);
-    
-    public static Jaguar RHConveyor = new Jaguar(4);
-    public static ConveyorProxSwitch RHTop = new ConveyorProxSwitch(4); //new DigitalInput(0);
-    public static ConveyorProxSwitch RHMiddle = new ConveyorProxSwitch(3); //new DigitalInput(0);
-    
+
     public static Jaguar Leg = new Jaguar(6);
-    
-    public static double ClimbPower = prefs.getDouble("ClimbPower", 0.5);
     public static double LegPower = prefs.getDouble("LegPower", 1);
     public static DigitalInput LegUp = new DigitalInput(13);
     public static DigitalInput LegDown = new DigitalInput(5);
@@ -58,11 +46,5 @@ public class RobotMap {
 
     
     // Controls
-    public static Joystick Joystick1 = null; //new Joystick(1);
     public static Joystick Joystick2 = new Joystick(1);
-    public static Joystick Joystick3 = null; //new Joystick(3);
-    
-    public static Servo CameraServo = new Servo(9);
-    
-    // public static NetworkTable cameraTable = NetworkTable.getTable("camera");
 }
